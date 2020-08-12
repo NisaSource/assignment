8ccabdec1d5fb51c38b9d958c5eb33f4c683333d
Khaerunnisa <nisa@js.local>
2020-08-12 13:25:43 +0800

    Initial routes setup

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hiiiii');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+//Reset state before starting tests
+app.post('/reset', (req, res) => {
+	console.log('Reset');
+	res.status(200).send('OK');
+});
+
+app.get('/balance', (req, res) => {
+	console.log('Balance');
+	res.status(404).send('0');
+});
+
+app.post('/event', (req, res) => {
+	console.log('Event');
+	res.status(201).send('Event');
+});
+
+app.listen(8000, () => {
+	console.log('Listening on 8000');
+});
